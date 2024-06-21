3674062db055ed197e1b46064a1e41b471af057e
Shihab Khan <khanshi@hhsc.ca>
2024-06-20 21:03:44 -0400

    Add project files.

---
+++ Listeners/Program.cs
@@ -0,0 +1,64 @@
+﻿// See https://aka.ms/new-console-template for more information
+// add TCP Listener
+
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+var tcpListener = new TcpListener(IPAddress.Loopback, 11514);
+await StartTcpListener();
+
+
+async Task StartTcpListener()
+{
+    try
+    {
+        tcpListener.Start();
+
+        using var handler = await tcpListener.AcceptTcpClientAsync();
+        await using var stream = handler.GetStream();
+
+        var buffer = new byte[1_024];
+        int received = await stream.ReadAsync(buffer);
+
+        var message = Encoding.UTF8.GetString(buffer, 0, received);
+        Console.WriteLine($"Message received: \"{message}\"");
+
+        //var data = Encoding.ASCII.GetBytes("hello");
+
+        //var cts = new CancellationTokenSource(TimeSpan.FromMilliseconds(25));
+
+        //if (cts.IsCancellationRequested)
+        //{
+        //    cts.Cancel();
+        //}
+
+        //await stream.WriteAsync(data, 0, data.Length, CancellationToken.None);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
+
+
+//async Task StartUdpListener()
+//{
+//    var listerEndpoint = new IPEndPoint(IPAddress.Loopback, 514);
+//    var udpClient = new UdpClient(listerEndpoint);
+
+//    udpClient.BeginReceive(new AsyncCallback(ReceiveCallBack), )
+//}
+
+//struct UdpState
+//{
+//    public UdpClient UdpClient { get; set; }
+//    public IPEndPoint IpEndPoint { get; set; }
+//}
+//void ReceiveCallBack(IAsyncResult asyncResult)
+//{
+//    var udpClient = asyncResult.AsyncState;
+//}
+
+// add UDP Listener
---
+++ MessageSenders/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net;
+using System.Net.Sockets;
+
+
+var data = await File.ReadAllBytesAsync(@"C:\audits\sample_audit2.txt");
+
+await SendTcpMessage(data);
+
+async Task SendTcpMessage(byte[] data)
+{
+
+    var tcpEndpoint = new IPEndPoint(IPAddress.Loopback, 11514);
+    using var tcpClient = new TcpClient();
+    await tcpClient.ConnectAsync(tcpEndpoint);
+
+    var stream = tcpClient.GetStream();
+
+    await stream.WriteAsync(data, 0, data.Length);
+
+    Console.WriteLine($"Audit sent to: {tcpEndpoint.Address}");
+
+    await Task.Delay(10);
+
+    //var data2 = new byte[256];
+
+    //await stream.ReadAsync(data2, 0, data2.Length, CancellationToken.None);
+
+    //Console.WriteLine(Encoding.ASCII.GetString(data2));
+
+    Console.ReadKey();
+}
+
+
+async Task SendUdpMessage()
+{
+    var client = new UdpClient();
+    var endpoint = new IPEndPoint(IPAddress.Loopback, 514);
+
+    await client.SendAsync(data, endpoint, CancellationToken.None);
+
+    Console.WriteLine($"Audit sent to: {endpoint.Address}");
+    Console.ReadKey();
+}
