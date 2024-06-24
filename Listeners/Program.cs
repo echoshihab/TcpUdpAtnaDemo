57efc70e552f72e5481281627f5431bea12e22cf
Shihab Khan <khanshi@hhsc.ca>
2024-06-24 13:29:38 -0400

    move TcpListener functionality to TcpReceiver

--- Listeners/Program.cs
+++ Listeners/Program.cs
@@ -1,15 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // add TCP Listener
 
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
+using Listener.Tcp;
 using Listener.Udp;
 
 
-var tcpListener = new TcpListener(IPAddress.Loopback, 11514);
-//await StartTcpListener();
-//StartUdpReceiver();
 
 Console.CancelKeyPress += (sender, e) =>
 {
@@ -18,30 +13,6 @@ Console.CancelKeyPress += (sender, e) =>
 };
 
 await UdpReceiver.ReceiveMessagesAsync();
+//await TcpReceiver.StartTcpListenerAsync();
 
-async Task StartTcpListener()
-{
-    try
-    {
-        tcpListener.Start();
-
-        using var handler = await tcpListener.AcceptTcpClientAsync();
-        await using var stream = handler.GetStream();
-
-        var buffer = new byte[1_024];
-        int received = await stream.ReadAsync(buffer);
-
-        var message = Encoding.UTF8.GetString(buffer, 0, received);
-        Console.WriteLine($"Message received: \"{message}\"");
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine(ex.Message);
-    }
-}
-
-void StartUdpReceiver()
-{
-    AsyncCallbackUdpReceiver.ReceiveMessages();
-}
 
---
+++ Listeners/Tcp/TcpReceiver.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace Listener.Tcp
+{
+    public static class TcpReceiver
+    {
+        private static TcpListener _tcpListener;
+        public static async Task StartTcpListenerAsync()
+        {
+            try
+            {
+                _tcpListener = new TcpListener(IPAddress.Loopback, 11514);
+                _tcpListener.Start();
+
+                using var handler = await _tcpListener.AcceptTcpClientAsync();
+                await using var stream = handler.GetStream();
+
+                var buffer = new byte[1_024];
+                int received = await stream.ReadAsync(buffer);
+
+                var message = Encoding.UTF8.GetString(buffer, 0, received);
+                Console.WriteLine($"Message received: \"{message}\"");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+    }
+}
