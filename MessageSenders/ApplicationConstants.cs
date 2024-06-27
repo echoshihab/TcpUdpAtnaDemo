5cdc53372028b8b6901e8957674418af330c88f8
Shihab Khan <khanshi@hhsc.ca>
2024-06-27 16:09:14 -0400

    update application constants

--- MessageSenders/ApplicationConstants.cs
+++ MessageSenders/ApplicationConstants.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MessageSender
+﻿namespace MessageSender
 {
     public class ApplicationConstants
     {
         public const int SYSLOG_AUDIT_LOG_FACILITY_CODE = 13;
         public const int SYSLOG_SECURITY_AUTH_FACILITY_CODE = 10;
+        public const int SYSLOG_SEVERITY_AUTHORIZATION = 4;
+        public const string AUDIT_ENTERPRISE_SITE_ID = "OUR";
+        public const string APPLICATION_IDENTIFIER = "ARR_DEMO";
+
     }
 }
